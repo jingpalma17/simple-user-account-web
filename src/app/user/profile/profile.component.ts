4bf7bf872a41456d6f4cdece49c1861c380e2605
Jingle Palma <jingpalma17@gmail.com>
2020-07-20 19:18:19 +0800

    Implement UI for profile page (Initial)

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      mobileNumber: new FormControl('', Validators.required),
+      birthday: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+    });
   }
 
+  update(data) {}
 }
