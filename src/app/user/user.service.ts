47166f12bad701990e867c41ab082c80c21a4052
Jingle Palma <jingpalma17@gmail.com>
2020-07-20 23:50:37 +0800

    Implement userService

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private basePath = 'users';
+  private accessToken: string = null;
+
+  constructor(private readonly http: HttpClient) {}
+
+  private getHeaders() {
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    if (this.accessToken) {
+      // Update header
+    }
+
+    return headers;
+  }
+
+  // TODO Add type
+  getAll(): Observable<any> {
+    const headers = new HttpHeaders(this.getHeaders());
+    const options = { headers };
+
+    return this.http.get(environment.apiBaseUrl + this.basePath, options);
+  }
+
+  getProfile(): Observable<any> {
+    const headers = new HttpHeaders(this.getHeaders());
+    const options = { headers };
+
+    return this.http.get(
+      environment.apiBaseUrl + `${this.basePath}/me`,
+      options
+    );
+  }
+
+  create(user) {
+    const headers = new HttpHeaders(this.getHeaders());
+    const options = { headers };
+
+    return this.http.post(
+      environment.apiBaseUrl + this.basePath,
+      user,
+      options
+    );
+  }
+
+  update(user) {
+    const headers = new HttpHeaders(this.getHeaders());
+    const options = { headers };
+    return this.http.put(
+      environment.apiBaseUrl + `${this.basePath}/${user.id}`,
+      user,
+      options
+    );
+  }
+
+  delete(id) {
+    const headers = new HttpHeaders(this.getHeaders());
+    const options = { headers };
+    return this.http.delete(
+      environment.apiBaseUrl + `${this.basePath}/${id}`,
+      options
+    );
+  }
+}
