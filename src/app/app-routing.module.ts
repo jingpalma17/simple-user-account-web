39e84af17e42e1333d0917df03e558b82124135d
Jingle Palma <jingpalma17@gmail.com>
2020-07-22 21:47:28 +0800

    Redirect to profile screen if authenticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'profile',
+      },
+      {
         path: 'profile',
         component: ProfileComponent,
       },
